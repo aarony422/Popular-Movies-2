4cfaa494263d212f9acdaa027df4c6dbb033a144
Aaron Young <aarony422@gmail.com>
2016-09-30 17:03:45 -0500

    added movie fragment, Picasso, added gridView in fragment

--- app/src/main/java/me/aaronyoung/popular_movies/MainActivity.java
+++ app/src/main/java/me/aaronyoung/popular_movies/MainActivity.java
@@ -1,13 +1,28 @@
 package me.aaronyoung.popular_movies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity implements MovieFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add movie fragment to main activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main, new MovieFragment())
+                    .commit();
+        }
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
+
 }
