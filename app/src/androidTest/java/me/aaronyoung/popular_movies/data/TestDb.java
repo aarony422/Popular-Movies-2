19fa1bd8061acc4ee92cc6aefe79f06a1c9810b4
Aaron Young <aarony422@gmail.com>
2016-12-08 15:52:20 -0600

    added JUnit Tests and started MovieDbHelper

---
+++ app/src/androidTest/java/me/aaronyoung/popular_movies/data/TestDb.java
@@ -0,0 +1,18 @@
+package me.aaronyoung.popular_movies.data;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by shangweiyoung on 12/8/16.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TestDb {
+    @Test
+    public void testTrue() {
+        assertEquals("hello", "hello");
+    }
+}
