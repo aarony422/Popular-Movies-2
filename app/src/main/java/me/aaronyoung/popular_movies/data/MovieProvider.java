e2e02151fa692d9ffab07d8e5c718fadefb5f0e9
Aaron Young <aarony422@gmail.com>
2017-01-10 11:54:38 -0600

    Started implementing method query. added 3 new URIs, one for each preference, to replace /movie/*

--- app/src/main/java/me/aaronyoung/popular_movies/data/MovieProvider.java
+++ app/src/main/java/me/aaronyoung/popular_movies/data/MovieProvider.java
@@ -2,6 +2,10 @@ package me.aaronyoung.popular_movies.data;
 
 import android.content.ContentProvider;
 import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+import static android.Manifest.permission_group.LOCATION;
 
 /**
  * Created by shangweiyoung on 1/10/17.
@@ -11,13 +15,22 @@ public class MovieProvider extends ContentProvider {
 
     // The URI Matcher used by this content provider.
     private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mOpenHelper;
 
     static final int MOVIE = 100;
     static final int MOVIE_WITH_ID = 101;
-    static final int MOVIE_WITH_PREFERENCE = 102;
+    static final int MOVIE_WITH_PREFERENCE_POPULAR = 102;
+    static final int MOVIE_WITH_PREFERENCE_TOP_RATED = 103;
+    static final int MOVIE_WITH_PREFERENCE_FAVORITE = 104;
     static final int TRAILERS = 200;
     static final int REVIEWS = 300;
     static final int FAVORITES = 400;
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MovieDbHelper(getContext());
+        return true;
+    }
 
     static UriMatcher buildUriMatcher() {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -26,11 +39,81 @@ public class MovieProvider extends ContentProvider {
         // For each type of URI you want to add, create a corresponding code.
         matcher.addURI(authority, MovieContract.PATH_MOVIES, MOVIE);
         matcher.addURI(authority, MovieContract.PATH_MOVIES + "/#", MOVIE_WITH_ID);
-        matcher.addURI(authority, MovieContract.PATH_MOVIES + "/*", MOVIE_WITH_PREFERENCE);
+        matcher.addURI(authority, MovieContract.PATH_MOVIES + "/popular", MOVIE_WITH_PREFERENCE_POPULAR);
+        matcher.addURI(authority, MovieContract.PATH_MOVIES + "/top_rated", MOVIE_WITH_PREFERENCE_TOP_RATED);
+        matcher.addURI(authority, MovieContract.PATH_MOVIES + "/favorite", MOVIE_WITH_PREFERENCE_FAVORITE);
         matcher.addURI(authority, MovieContract.PATH_TRAILERS, TRAILERS);
         matcher.addURI(authority, MovieContract.PATH_REVIEWS, REVIEWS);
         matcher.addURI(authority, MovieContract.PATH_FAVORITES, FAVORITES);
 
         return matcher;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Here's the switch statement that, given a URI, will determine what kind of request it is,
+        // and query the database accordingly.
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+
+            // "movie"
+            case MOVIE: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "trailers"
+            case TRAILERS: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.TrailerEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "reviews"
+            case REVIEWS: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.ReviewEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "trailers"
+            case FAVORITES: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.FavoriteEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 }
